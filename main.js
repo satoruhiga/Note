e3bbedb1827c95d878863f0f711bc0c8a4323449
Satoru Higa <satoruhiga@gmail.com>
2016-06-29 15:41:43 +0900

    Add keyboard shortcut

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const electron = require('electron')
 const app = electron.app
+
 const BrowserWindow = electron.BrowserWindow
+const Menu = electron.Menu;
 
 const storage = require('electron-json-storage');
 const chokidar = require('chokidar');
@@ -72,6 +74,27 @@ function createWindow() {
 			mainWindow.on('close', () => {
 				storage.set('window-size', mainWindow.getBounds(), (e) => { });
 			});
+
+			var template = [{
+				label: "Application",
+				submenu: [
+					{ label: "About Note", selector: "orderFrontStandardAboutPanel:" },
+					{ type: "separator" },
+					{ label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+				]}, {
+				label: "Edit",
+				submenu: [
+					{ label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+					{ label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+					{ type: "separator" },
+					{ label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+					{ label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+					{ label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+					{ label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+				]}
+			];
+
+			Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 		});
 	});
 }
